eabc347514aa8c3703fc3f5852ba4e25dffebdeb
philipAvdey <91579036+philipAvdey@users.noreply.github.com>
2024-08-06 16:06:02 -0700

    Redid build, updated project id, etc

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,7 +2,7 @@ import { createClient }  from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+    projectId: '9va1pavq',
     dataset: 'production',
     apiVersion: '2022-02-01',
     useCdn: true,
