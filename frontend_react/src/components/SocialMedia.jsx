9b34fc0222faf27c0b267069e7bdb174ef0c5af6
philipAvdey <91579036+philipAvdey@users.noreply.github.com>
2024-09-01 16:24:57 -0700

    added social media functionality

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import { BsTwitter, BsInstagram } from 'react-icons/bs'
-import { FaFacebookF } from 'react-icons/fa'
+import React from "react";
+import { BsTwitter, BsInstagram } from "react-icons/bs";
+import { FaLinkedin } from "react-icons/fa";
 
 const SocialMedia = () => {
   return (
-    <div className='app__social'>
-        <div>
-            <BsTwitter />
-        </div>
-        <div>
-            <FaFacebookF />
-        </div>
-        <div>
-            <BsInstagram />
-        </div>
+    <div className="app__social">
+      <div>
+        <a href="https://www.linkedin.com/in/philip-avdey-138428250/" target="_blank">
+        <FaLinkedin />
+        </a>
+      </div>
+      <div>
+        <a href="https://x.com/PhilipAvdey" target="_blank">
+          <BsTwitter />
+        </a>
+      </div>
+      <div>
+        <a href="https://instagram.com/philip.avdey/" target="_blank">
+          <BsInstagram />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SocialMedia
+export default SocialMedia;
